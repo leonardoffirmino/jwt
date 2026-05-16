2e5f416e5ad9ea3af10f556903d387547cbef0bb
leonardoffirmino <firmino750@gmail.com>
2026-05-16 17:41:00 -0300

    feat: create interfaces the controllers

--- src/controllers/balance_editor.py
+++ src/controllers/balance_editor.py
@@ -1,11 +1,11 @@
-from typing import Dict
 from src.models.interface.user_repository import UserRepositoryInterface
+from .interfaces.balance_editor  import BalanceEditorInterface
 
-class BalanceEditor:
+class BalanceEditor(BalanceEditorInterface):
     def __init__(self, user_repository: UserRepositoryInterface) -> None:
         self.__user_repository = user_repository
 
-    def edit(self, user_id: int, new_balance: float) -> Dict:
+    def edit(self, user_id: int, new_balance: float) -> dict:
         self.__user_repository.edit_balance(user_id, new_balance)
         return {
             "type": "User",
